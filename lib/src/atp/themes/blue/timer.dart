d700ec1a7394989ab5c3a8ba8448735996a3acdc
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-05-31 14:04:16 +0530

    started flutter provider pattern

--- lib/src/atp/themes/blue/timer.dart
+++ lib/src/atp/themes/blue/timer.dart
@@ -3,9 +3,9 @@ import 'package:pauzr/src/atp/screens/timer.dart';
 
 Timer timerTheme = Timer(
   backgroundColor: Color(0xff70b3dd),
-  quoteBackgroundColor: Color(0xff0D62A2),
-  cardTopBackgroundColor: Colors.black,
-  cardBottomBackgroundColor: Colors.black,
+  quoteBackgroundColor: Colors.blue.shade900,
+  cardTopBackgroundColor: Colors.white,
+  cardBottomBackgroundColor: Colors.blue.shade900,
   navigationBackgroundColor: Color(0xff70b3dd),
   navigationButtonBackgroundColor: Color(0xff0D62A2),
   navigationColor: Color(0xff0D62A2),
--- lib/src/atp/themes/red/timer.dart
+++ lib/src/atp/themes/red/timer.dart
@@ -4,8 +4,8 @@ import 'package:pauzr/src/atp/screens/timer.dart';
 Timer timerTheme = Timer(
   backgroundColor: Colors.red.shade100,
   quoteBackgroundColor: Colors.red,
-  cardTopBackgroundColor: Colors.black,
-  cardBottomBackgroundColor: Colors.black,
+  cardTopBackgroundColor: Colors.white,
+  cardBottomBackgroundColor: Colors.brown,
   navigationBackgroundColor: Colors.red.shade100,
   navigationButtonBackgroundColor: Colors.red,
   navigationColor: Colors.red,
