e32040c9e9b209a2ed78f7168c4f5e0910b79ec6
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-17 14:45:10 +0530

    init

--- lib/src/helpers/vars.dart
+++ lib/src/helpers/vars.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart' show kReleaseMode;
 
 String appName = "PauzR";
-String appId = "https://play.google.com/store/apps/details?id=com.whatsapp";
+String appId = "https://play.google.com/store/apps/details?id=com.pauzr.org";
 
 String baseUrl =
     kReleaseMode ? "https://www.pauzr.com" : "http://192.168.2.200:8000";
