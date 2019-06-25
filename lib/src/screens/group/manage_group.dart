b403ca218a9dd18fb93037c58d63f2b7bdc3c323
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-25 10:58:00 +0530

    init

--- lib/src/screens/group/manage_group.dart
+++ lib/src/screens/group/manage_group.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pauzr/src/atp/default.dart';
 import 'package:pauzr/src/helpers/fonts.dart';
@@ -180,7 +183,14 @@ class _ManageGroupPageState extends State<ManageGroupPage> {
   }
 
   void uploadGroupImage() async {
-    final file = await ImagePicker.pickImage(source: ImageSource.gallery);
+    final File image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    final File file = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      ratioX: 1.0,
+      ratioY: 1.0,
+      maxWidth: 512,
+      maxHeight: 512,
+    );
 
     setState(() {
       loading = true;
