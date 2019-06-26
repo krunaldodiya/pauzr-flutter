8c85289f18b7284bbd901f56e9fd46bc1af0aee1
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-26 11:38:48 +0530

    init

--- lib/src/models/timer.dart
+++ lib/src/models/timer.dart
@@ -1,10 +1,11 @@
 import 'package:meta/meta.dart';
+import 'package:pauzr/src/models/city.dart';
 
 @immutable
 class Timer {
   final int id;
   final int userId;
-  final int locationId;
+  final City city;
   final String duration;
   final String createdAt;
   final String updatedAt;
@@ -12,7 +13,7 @@ class Timer {
   Timer({
     this.id,
     this.userId,
-    this.locationId,
+    this.city,
     this.duration,
     this.createdAt,
     this.updatedAt,
@@ -22,7 +23,7 @@ class Timer {
     return Timer(
       id: json["id"] ?? this.id,
       userId: json["user_id"] ?? this.userId,
-      locationId: json["location_id"] ?? this.locationId,
+      city: json["city"] ?? this.city,
       duration: json["duration"] ?? this.duration,
       createdAt: json["created_at"] ?? this.createdAt,
       updatedAt: json["updated_at"] ?? this.updatedAt,
@@ -33,7 +34,9 @@ class Timer {
     return Timer(
       id: json["id"] != null ? json["id"] : null,
       userId: json["user_id"] != null ? json["user_id"] : null,
-      locationId: json["location_id"] != null ? json["location_id"] : null,
+      city: json["city"] != null
+          ? json["city"] is City ? json["city"] : City.fromMap(json["city"])
+          : null,
       duration: json["duration"] != null ? json["duration"] : null,
       createdAt: json["created_at"] != null ? json["created_at"] : null,
       updatedAt: json["updated_at"] != null ? json["updated_at"] : null,
